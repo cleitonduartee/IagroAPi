bc33dddb69763656b16caa31e42f7c165fcf2d49
cleitonduartee <djcleitonduarte@hotmail.com>
2022-01-31 20:13:35 -0400

    Criado class 'ApiContext' e config iniciais com entidade 'Municipio'

---
+++ Infraestrutura/Configuracao/ApiContext.cs
@@ -0,0 +1,26 @@
+﻿using Entidades.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestrutura.Configuracao
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+        public DbSet<Municipio> Municipios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(ObterStringConexaoBanco());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        public static string ObterStringConexaoBanco()
+        {
+            return "Host=localhost;Port=5432;Pooling=true;Database=IagroDB;User Id=postgres;Password=123;";
+        }
+    }
+}
