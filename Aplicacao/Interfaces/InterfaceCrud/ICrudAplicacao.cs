b8a18dcf1489ff70c20432f1013a53b0bafaa3cc
cleitonduartee <djcleitonduarte@hotmail.com>
2022-01-31 20:29:14 -0400

    Criado interface IMunicipio na camada 'Aplicacao'

---
+++ Aplicacao/Interfaces/InterfaceCrud/ICrudAplicacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Interfaces.InterfaceCrud
+{
+    public interface ICrudAplicacao<T> where T : class
+    {
+        Task Adicionar(T obj);
+        Task Atualizar(T obj);
+        Task Excluir(T obj);
+        Task<T> BuscarPorId(int Id);
+        Task<List<T>> BuscarTodos();
+    }
+}
