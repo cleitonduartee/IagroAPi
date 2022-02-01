c17ab48580409f614df9d989335fd0f8345e7f73
cleitonduartee <djcleitonduarte@hotmail.com>
2022-01-31 20:18:56 -0400

    Criado interface IMunicipio no projeto 'Dominio'

---
+++ Dominio/Interfaces/IMunicipio.cs
@@ -0,0 +1,9 @@
+﻿using Dominio.Interfaces.InterfaceCrud;
+using Entidades.Entidades;
+
+namespace Dominio.Interfaces
+{
+    public interface IMunicipio : ICrud<Municipio>
+    {
+    }
+}
---
+++ Dominio/Interfaces/InterfaceCrud/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.InterfaceCrud
+{
+    public interface ICrud<T> where T : class
+    {
+        Task Adicionar(T obj);
+        Task Atualizar(T obj);
+        Task Excluir(T obj);
+        Task<T> BuscarPorId(int Id);
+        Task<List<T>> BuscarTodos();
+    }
+}
