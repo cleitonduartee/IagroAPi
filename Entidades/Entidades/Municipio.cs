58337ec5af67d34601b3b9749eee3eeb19f56211
cleitonduartee <djcleitonduarte@hotmail.com>
2022-01-31 20:07:03 -0400

    Criado primeira Classe 'Municipio'

---
+++ Entidades/Entidades/Municipio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Entidades.Entidades
+{
+    [Table("tb_municipio")]
+    public class Municipio
+    {
+        [Key]
+        public int Id { get; protected set; }
+        public string Nome { get; set; }
+
+        public Municipio()
+        {
+        }
+
+        public Municipio(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
